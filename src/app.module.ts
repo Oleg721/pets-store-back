90a2d43e9aa6c0c9bd78efd0ce20cb0e2d257193
Marina <marina.p@carbonclick.com>
2024-04-24 02:10:51 +0200

    Add User CRUD operations, set up nodemon, swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './user/users.module';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost',
+			port: 5432,
+			username: 'postgres',
+			password: 'Flo1lilly95',
+			database: 'pets-db', // db name
+			// entities: [User],
+			autoLoadEntities: true, // to automatically load entities
+			synchronize: true, // shouldn't be used in production - otherwise we can lose production data.
+		}),
+		UsersModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
