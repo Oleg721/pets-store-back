d172ab1decd9eef34edd4b496fc7ded086c50052
Oleg721 <novikov.oleg721@gmail.com>
2024-04-25 22:37:37 +0200

    Added rest entities

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,2 +1,6 @@
 export { User } from './user.entity';
 export { Product } from './product.entity';
+export { AttributeName } from './attributeName.entity';
+export { Category } from './category.entity';
+export { ProductAttributeName } from './productAttributeName.entity';
+export { CategoryAttribute } from './categoryAttribute.entity';
