0ece48ccc61c41619f29585db8f411704049a0b8
Oleg721 <novikov.oleg721@gmail.com>
2024-04-25 18:42:53 +0200

    Added database module with repositories

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,11 +1,24 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  database: {
-    type: process.env.DB_TYPE || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'pets-db',
-  }
+import { DataSourceOptions } from 'typeorm';
+
+type DefaultAppConfig = {
+	port: number;
+	database: DataSourceOptions;
+};
+
+type dbType = 'postgres' | 'mysql' | 'mariadb' | 'sqlite' | 'mssql';
+
+const isCorrectType = (type: string): type is dbType => {
+	return ['mysql', 'mariadb', 'postgres', 'sqlite', 'mssql'].includes(type);
+};
+
+export default (): DefaultAppConfig => ({
+	port: parseInt(process.env.PORT, 10) || 3000,
+	database: {
+		type: isCorrectType(process.env.DB_TYPE) ? process.env.DB_TYPE : 'postgres',
+		host: process.env.DB_HOST || 'localhost',
+		port: parseInt(process.env.DB_PORT) || 5432,
+		username: process.env.DB_USERNAME || 'postgres',
+		password: process.env.DB_PASSWORD || '',
+		database: process.env.DB_NAME || 'pets-db',
+	},
 });
