e7386a2f3128c32c7ff0ed5f00c5e65e6bb2819c
Oleg721 <novikov.oleg721@gmail.com>
2024-06-04 16:38:20 +0200

    Add CategoryAttributeModule for import and remove CategoryAttributeService for related modules

--- src/resources/category/category.module.ts
+++ src/resources/category/category.module.ts
@@ -3,18 +3,20 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { CategoryMapperProvider } from './categoryMapper.provider';
-import { CategoryAttributeService } from '../category-attribute/category-attribute.service';
 import { AttributeNameModule } from '../attribute-name/attribute-name.module';
 import { ProductAttributeNameModule } from '../product-attribute-name/product-attribute-name.module';
 import { ProductModule } from '../product/product.module';
+import { CategoryAttributeModule } from '../category-attribute/category-attribute.module';
 
 @Module({
-	imports: [DatabaseModule, AttributeNameModule, ProductAttributeNameModule, ProductModule],
-	controllers: [CategoryController],
-	providers: [
-		CategoryService,
-		CategoryMapperProvider,
-		CategoryAttributeService,
+	imports: [
+		DatabaseModule,
+		AttributeNameModule,
+		ProductAttributeNameModule,
+		ProductModule,
+		CategoryAttributeModule,
 	],
+	controllers: [CategoryController],
+	providers: [CategoryService, CategoryMapperProvider],
 })
 export class CategoryModule {}
--- src/resources/product/product.module.ts
+++ src/resources/product/product.module.ts
@@ -4,18 +4,18 @@ import { ProductController } from './product.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { ProductMapperProvider } from './productMapper.provider';
 import { CategoryService } from '../category/category.service';
-import { CategoryAttributeService } from '../category-attribute/category-attribute.service';
 import { AttributeNameModule } from '../attribute-name/attribute-name.module';
 import { ProductAttributeNameModule } from '../product-attribute-name/product-attribute-name.module';
+import { CategoryAttributeModule } from '../category-attribute/category-attribute.module';
 
 @Module({
-	imports: [DatabaseModule, AttributeNameModule, ProductAttributeNameModule],
-	providers: [
-		ProductService,
-		ProductMapperProvider,
-		CategoryService,
-		CategoryAttributeService,
+	imports: [
+		DatabaseModule,
+		AttributeNameModule,
+		ProductAttributeNameModule,
+		CategoryAttributeModule,
 	],
+	providers: [ProductService, ProductMapperProvider, CategoryService],
 	controllers: [ProductController],
 	exports: [ProductService, ProductMapperProvider],
 })
