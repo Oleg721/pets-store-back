b87f724377801b8265db4c392f4ef89cfaf5455b
Oleg721 <novikov.oleg721@gmail.com>
2024-06-04 16:32:29 +0200

    Add ProductModule for import and remove ProductService, ProductMapperProvider for CategoryModule

--- src/resources/category/category.module.ts
+++ src/resources/category/category.module.ts
@@ -4,20 +4,17 @@ import { CategoryController } from './category.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { CategoryMapperProvider } from './categoryMapper.provider';
 import { CategoryAttributeService } from '../category-attribute/category-attribute.service';
-import { ProductService } from '../product/product.service';
-import { ProductMapperProvider } from '../product/productMapper.provider';
 import { AttributeNameModule } from '../attribute-name/attribute-name.module';
 import { ProductAttributeNameModule } from '../product-attribute-name/product-attribute-name.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
-	imports: [DatabaseModule, AttributeNameModule, ProductAttributeNameModule],
+	imports: [DatabaseModule, AttributeNameModule, ProductAttributeNameModule, ProductModule],
 	controllers: [CategoryController],
 	providers: [
 		CategoryService,
 		CategoryMapperProvider,
-		ProductMapperProvider,
 		CategoryAttributeService,
-		ProductService,
 	],
 })
 export class CategoryModule {}
